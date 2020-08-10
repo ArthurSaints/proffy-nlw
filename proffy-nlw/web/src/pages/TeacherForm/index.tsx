7ec6d42203293387c65f02c97240f7ec2ab76742
Arthur Santos <arthursantosgc@gmail.com>
2020-08-10 12:08:21 -0300

    Novos componentes criados {Inputs, Select, Textarea}

--- proffy-nlw/web/src/pages/TeacherForm/index.tsx
+++ proffy-nlw/web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,63 @@
-import React from "react"
-import PageHeader from "../../components/PageHeader"
+import React from 'react'
+
+import PageHeader from '../../components/PageHeader'
+import Input from '../../components/Inputs'
+import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
+
+import warningIcon from '../../assets/images/icons/warning.svg'
+
+import './styles.css'
 
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas!" />
+            <PageHeader
+                title="Que incrível que você quer dar aulas!"
+                description="O primeiro passo é preencher esse formulário de inscrição."
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biografia" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Educação física', label: 'Educação física' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Programação', label: 'Programação' }
+                        ]}
+                    />
+
+                    <Input name="cost" label="Custo da sua aula por hora" />
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante!" />
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
         </div>
     )
 }
--- proffy-nlw/web/src/pages/TeacherList/index.tsx
+++ proffy-nlw/web/src/pages/TeacherList/index.tsx
@@ -1,27 +1,18 @@
 import PageHeader from "../../components/PageHeader"
 import React from 'react'
-import './style.css'
+import './styles.css'
 import TeacherItem from "../../components/TeacherItem"
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
-
+import Input from "../../components/Inputs"
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponiveis:">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da semana</label>
-                        <input type="text" id="subject" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Input name="subject" label="Matéria" />
+                    <Input name="week_day" label="Dia da semana" />
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
             <main>
